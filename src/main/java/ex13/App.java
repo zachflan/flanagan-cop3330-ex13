29ba8fda054d0c2261b3b8b53bf9c1554f6a3e01
Zachary Flanagan <zacharyf24147@gmail.com>
2021-06-06 20:02:12 -0400

    Commit 2

--- src/main/java/ex13/App.java
+++ src/main/java/ex13/App.java
@@ -1,4 +1,40 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 13 Solution
+ *  Copyright 2021 Zachary Flanagan
+ */
+
 package ex13;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class App {
+    public static void main(String[] args) {
+
+        Scanner principle = new Scanner(System.in);
+        System.out.print("Enter the principal: ");
+        String principleS = principle.nextLine();
+        double principleD = Double.parseDouble(principleS);
+
+        Scanner roi = new Scanner(System.in);
+        System.out.print("Enter the rate of interest: ");
+        String roiS = roi.nextLine();
+        double roiD = Double.parseDouble(roiS);
+        double percentRoid = roiD / 100;
+
+        Scanner years = new Scanner(System.in);
+        System.out.print("Enter the number of years: ");
+        String yearsS = years.nextLine();
+        double yearsI = Double.parseDouble(yearsS);
+
+        Scanner compoundsYear = new Scanner(System.in);
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        String compoundsYearS = compoundsYear.nextLine();
+        int compoundsYearI = Integer.parseInt(compoundsYearS);
+
+        double interest = principleD * Math.pow(1 + (percentRoid / compoundsYearI), (compoundsYearI * yearsI));
+
+//        System.out.format("After %f years at %.2f%%, the investment will be worth $%.2f.", yearsI, roiD, interest);
+        System.out.format("$%.2f invested at %.2f%% for %.0f years compounded %d times per year is $%.2f", principleD, roiD, yearsI, compoundsYearI, interest);
+    }
 }
